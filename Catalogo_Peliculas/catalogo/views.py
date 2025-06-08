bdf9639675afa1b7e9815a1d3a5faf486c50d569
RodrigoApaza <rodrigoantonioemci2016@gmail.com>
2025-06-07 22:40:50 -0400

    se puede ver lo que edité?

--- Catalogo_Peliculas/catalogo/views.py
+++ Catalogo_Peliculas/catalogo/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 from django .views.generic import TemplateView
 # Create your views here.
 
+from django.http import HttpResponse
 
+def index(request):
+    return HttpResponse("¡Bienvenido al Catálogo de Películas!")
