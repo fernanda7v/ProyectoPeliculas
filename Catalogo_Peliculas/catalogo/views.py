f14beb8fc97e1c32fe69f93c9a8aa312ea0a861d
hyota <manimamani04@gmail.com>
2025-06-07 22:58:54 -0400

    creando la funcion home

--- Catalogo_Peliculas/catalogo/views.py
+++ Catalogo_Peliculas/catalogo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django .views.generic import TemplateView
+from django.views.generic import TemplateView
 # Create your views here.
 
 
+def home(request):
+    return render(request,'catalogo/principal.html')
