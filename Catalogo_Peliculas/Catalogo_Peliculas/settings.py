5c78a5b89d36461ad8151e1bef53f2c1056a662c
iFernandezE <ivan.fernandez.esp@hotmail.com>
2025-06-16 00:59:00 -0400

    estilos finales agregados

--- Catalogo_Peliculas/Catalogo_Peliculas/settings.py
+++ Catalogo_Peliculas/Catalogo_Peliculas/settings.py
@@ -12,12 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-LOGIN_URL = '/login/'  # O lo que corresponda en tu proyecto
-
+LOGIN_URL = '/login/' 
+LOGIN_REDIRECT_URL = '/catalogo/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
